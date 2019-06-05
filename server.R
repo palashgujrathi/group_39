d55d176ba7482c464127dda780cae222ece459bf
linzykeeke <linzykeeke@gmail.com>
2019-06-05 15:56:18 -0700

    change the plot for minimum wage

--- server.R
+++ server.R
@@ -10,23 +10,40 @@ data <- read.csv("~/Desktop/INFO/Final Project/Minimum Wage Data.csv")
 # Define server logic required to draw a histogram
 shinyServer <- function(input, output) {
     
+    # output$highPlot <- renderPlotly({
+    #     state_name <- filter(data, State==input$states)
+    #     year_selected <- filter(state_name, Year>=input$years[1] & Year<=input$years[2])
+    #     x <- year_selected$Year
+    #     y <- year_selected$High.Value
+    #     p <- plot_ly(year_selected, x = ~x, y = ~y, colors = "#FF8C00", type = 'scatter', mode = 'lines')
+    #     return(p)
+    # })
+    # 
+    # output$lowPlot <- renderPlotly({
+    #     state_name <- filter(data, State==input$states)
+    #     year_selected <- filter(state_name, Year>=input$years[1] & Year<=input$years[2])
+    #     x <- year_selected$Year
+    #     y <- year_selected$Low.Value
+    #     p <- plot_ly(year_selected, x = ~x, y = ~y, color = "##e5541e", type = 'scatter', mode = 'lines')
+    #     return(p)
+    #})
+    
+    ## New plot that shows Minimum wage overall: 
     output$highPlot <- renderPlotly({
-        state_name <- filter(data, State==input$states)
-        year_selected <- filter(state_name, Year>=input$years[1] & Year<=input$years[2])
+        state_name <- filter(data, State %in% input$states)
+        year_selected <- filter(state_name, Year>=input$years[1], Year<=input$years[2])
         x <- year_selected$Year
         y <- year_selected$High.Value
-        p <- plot_ly(year_selected, x = ~x, y = ~y, colors = "#FF8C00", type = 'scatter', mode = 'lines')
+        p <- plot_ly(year_selected, x = ~x, y = ~y, color = ~year_selected$State, type = 'scatter', mode = 'lines') %>%
+            layout(xaxis = list(title = "YEARS"), yaxis = list(title = "MINIMUM WAGE ($)"),
+                   title = paste("MINIMUM WAGE ($) vs YEARS", min(input$years), "-", max(input$years)), margin = list(t=120))#FF8C00
+        
+        # p <- ggplot(year_selected) +
+        #   geom_line(aes(x = Year, y = High.Value, color = State)) +
+        #   ggtitle("CPI vs Time") + theme(plot.title = element_text(hjust = 0.5, size=16))
         return(p)
     })
-    
-    output$lowPlot <- renderPlotly({
-        state_name <- filter(data, State==input$states)
-        year_selected <- filter(state_name, Year>=input$years[1] & Year<=input$years[2])
-        x <- year_selected$Year
-        y <- year_selected$Low.Value
-        p <- plot_ly(year_selected, x = ~x, y = ~y, color = "##e5541e", type = 'scatter', mode = 'lines')
-        return(p)
-    })
+
     
     output$mapOutput <- renderPlot({
         state_name <- filter(data, Year==input$year)
